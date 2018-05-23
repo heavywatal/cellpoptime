de5a34b14bf4de31e448b61494b096b5ddf7240c
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-05-23 18:35:49 +0900

    :art: Clean sandbox.R

--- demo/sandbox.R
+++ demo/sandbox.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(wtl)
+library(tumopp)
 library(ggtree)
 refresh('cellpoptime')
 
@@ -12,22 +13,36 @@ refresh('cellpoptime')
 
 .param = c('-D2', '-k100', '-N16', '-U8', '--mb=99', '--ms1mut')
 samples = purrr::rerun(4L, {
-  (tumopp::ms(16L, .param) %>% wtl::parse_ms())[[1L]]
+  (tumopp::mslike(16L, .param) %>% wtl::parse_ms())[[1L]]
 }) %>% print()
 
 # samples = wtl::ms(6L, 4L, theta = 100) %>% wtl::parse_ms() %>% print()
-trees = samples %>% purrr::map(infer_rooted_tree) %>% print()
+trees = samples %>% purrr::map(cellpoptime::infer_rooted_tree) %>% print()
 trees %>%
-  as_multiphylo() %>%
+  cellpoptime::as_multiphylo() %>%
   .ggtree() +
   facet_wrap(~.id)
 
+.base = trees[[3]] %>% cellpoptime::remove_outgroup() %>% print()
+
+add_extra_columns = function(x) {
+  x %>%
+    dplyr::arrange(parent) %>%
+    dplyr::mutate(
+      is_tip = !is.na(label),
+      mutations = branch.length,
+      branch.length = pmax(branch.length, 0.01),
+      term_length = 0,
+      children = list(NULL)
+    )
+}
+.base %>% add_extra_columns()
+
 filter_scale_tips = function(x) {
   x %>%
-    dplyr::filter(!is.na(branch.length)) %>%
+    dplyr::filter(is_tip) %>%
     dplyr::group_by(parent) %>%
-    dplyr::filter(all(is_tip)) %>%
-    print() %>%
+    dplyr::filter(n() > 1L) %>%
     dplyr::mutate(total_length = min(branch.length + term_length)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(
@@ -40,14 +55,15 @@ filter_scale_tips = function(x) {
     dplyr::mutate(term_length = total_length) %>%
     dplyr::select(-scale, -total_length)
 }
+.base %>% add_extra_columns() %>% filter_scale_tips()
 
 nest_tippairs = function(x) {
   nested = filter_scale_tips(x) %>%
-    print() %>%
     tidyr::nest(-parent, -term_length, .key="children") %>%
     print()
   x %>%
     dplyr::filter(is.na(branch.length) | !parent %in% nested$parent) %>%
+    print() %>%
     dplyr::left_join(nested, by=c(node="parent"), suffix=c("", ".y")) %>%
     dplyr::mutate(
       is_tip = is_tip | node %in% nested$parent,
@@ -55,6 +71,7 @@ nest_tippairs = function(x) {
       children = ifelse(purrr::map_lgl(children, is.null), children.y, children),
       term_length.y = NULL, children.y = NULL)
 }
+.base %>% add_extra_columns() %>% nest_tippairs() #%>% nest_tippairs() %>% nest_tippairs()
 
 unnest_children = function(x) {
   .outer = x %>%
@@ -62,20 +79,11 @@ unnest_children = function(x) {
     dplyr::filter(!purrr::map_lgl(children, is.null)) %>%
     tidyr::unnest()
   .inner = x %>% dplyr::mutate(children=list(NULL))
-  bind_rows(.outer, .inner) %>%
+  dplyr::bind_rows(.outer, .inner) %>%
     dplyr::mutate(is_tip = !is.na(label))
 }
 
 rescale_branches = function(x) {
-  x = x %>%
-    dplyr::arrange(parent) %>%
-    dplyr::mutate(
-      is_tip = !is.na(label),
-      mutations = branch.length,
-      branch.length = pmax(branch.length + 0.01),
-      term_length = 0,
-      children = list(NULL)
-    )
   num_edges = nrow(x)
   while (nrow(x) > 1L) {
     x = nest_tippairs(x)
@@ -84,16 +92,15 @@ rescale_branches = function(x) {
     x = unnest_children(x)
   }
   x %>%
-    dplyr::select(-term_length, -children, -is_tip) %>%
+    # dplyr::select(parent, node, branch.length, label) %>%
     dplyr::arrange(node)
 }
 
-.base = trees[[4]] %>% remove_outgroup() %>% print()
-.shrunk = .base %>% rescale_branches() %>% print()
+.shrunk = .base %>% add_extra_columns() %>% rescale_branches() %>% print()
 
 .p = list(.base, .shrunk) %>%
   as_multiphylo() %>%
   .ggtree() +
   facet_wrap(~.id, ncol=1)
-
+.p
 ggsave('scaletree.pdf', .p, width=7, height=9.9)
