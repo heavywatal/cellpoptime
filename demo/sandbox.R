6fc48219bb7c3698cc58adb63b10b6b870ee0354
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-06-11 05:46:01 +0900

    :construction: Prepare slides for NCC meeting

--- demo/sandbox.R
+++ demo/sandbox.R
@@ -11,9 +11,10 @@ refresh('cellpoptime')
   wtl::theme_wtl()
 }
 
-.param = c('-D2', '-k100', '-N16', '-U8', '--mb=99', '--ms1mut')
+N = 20L
+.param = c('-D2', '-k100', paste0('-N', N), '-U10', '--mb=99', '--ms1mut', '-u0')
 samples = purrr::rerun(4L, {
-  (tumopp::mslike(16L, .param) %>% wtl::parse_ms())[[1L]]
+  (tumopp::mslike(N, .param) %>% wtl::parse_ms())[[1L]]
 }) %>% print()
 
 # samples = wtl::ms(6L, 4L, theta = 100) %>% wtl::parse_ms() %>% print()
@@ -23,86 +24,16 @@ trees %>%
   .ggtree() +
   facet_wrap(~.id)
 
-.base = trees[[3]] %>% cellpoptime::remove_outgroup() %>% print()
+.tree = trees[[1]]
+.base = .tree %>% cellpoptime::remove_outgroup() %>% print()
 
-add_extra_columns = function(x) {
-  x %>%
-    dplyr::arrange(parent) %>%
-    dplyr::mutate(
-      is_tip = !is.na(label),
-      mutations = branch.length,
-      branch.length = pmax(branch.length, 0.01),
-      term_length = 0,
-      exp_desc = ifelse(is_tip, 1L, NA_integer_), # expected number of descendant cells
-      children = list(NULL)
-    )
-}
 .base %>% add_extra_columns()
-
-filter_scale_tips = function(x) {
-  x %>%
-    dplyr::filter(is_tip) %>%
-    dplyr::group_by(parent) %>%
-    dplyr::filter(n() > 1L) %>%
-    dplyr::mutate(total_length = min(branch.length + term_length)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(
-      scale = total_length / (branch.length + term_length),
-      branch.length = branch.length * scale,
-      term_length = term_length * scale,
-      children = purrr::map2(children, scale, rescale_children)
-    ) %>%
-    print() %>%
-    dplyr::mutate(term_length = total_length) %>%
-    dplyr::select(-scale, -total_length)
-}
 .base %>% add_extra_columns() %>% filter_scale_tips()
 
-p_binom = function(events) {
-  pbinom(min(events), sum(events), 0.5)
-}
 p_binom(c(1, 8))
-
-detect_driver = function(exp_desc) {
-  p = exp_desc * 0 + 1
-  p[which.max(exp_desc)] = p_binom(exp_desc - 1)
-  p
-}
 detect_driver(c(1, 8))
 
-nest_tippairs = function(x) {
-  nested = filter_scale_tips(x) %>%
-    dplyr::group_by(parent, term_length) %>%
-    dplyr::mutate(p_driver = detect_driver(exp_desc)) %>%
-    tidyr::nest(.key="children") %>%
-    dplyr::mutate(exp_desc = purrr::map_int(children, ~{
-      #TODO weight by mutatoins
-      print(.x$exp_desc)
-      sum(.x$exp_desc)
-    })) %>%
-    print()
-  x %>%
-    dplyr::filter(is.na(branch.length) | !parent %in% nested$parent) %>%
-    print() %>%
-    dplyr::left_join(nested, by=c(node="parent"), suffix=c("", ".y")) %>%
-    dplyr::mutate(
-      is_tip = is_tip | node %in% nested$parent,
-      term_length = pmax(term_length, term_length.y, na.rm=TRUE),
-      exp_desc = dplyr::coalesce(exp_desc, exp_desc.y),
-      children = ifelse(purrr::map_lgl(children, is.null), children.y, children),
-      term_length.y = NULL, exp_desc.y = NULL, children.y = NULL)
-}
 .base %>% add_extra_columns() %>% nest_tippairs() #%>% nest_tippairs() %>% nest_tippairs()
-
-unnest_children = function(x) {
-  .outer = x %>%
-    dplyr::transmute(parent=node, children) %>%
-    dplyr::filter(!purrr::map_lgl(children, is.null)) %>%
-    tidyr::unnest()
-  .inner = x %>% dplyr::mutate(children=list(NULL))
-  dplyr::bind_rows(.outer, .inner) %>%
-    dplyr::mutate(is_tip = !is.na(label))
-}
 
 rescale_branches = function(x) {
   num_edges = nrow(x)
@@ -112,25 +43,98 @@ rescale_branches = function(x) {
   while (nrow(x) < num_edges) {
     x = unnest_children(x)
   }
-  x %>%
-    # dplyr::select(parent, node, branch.length, label) %>%
-    dplyr::arrange(node)
+  x %>% dplyr::arrange(node)
 }
 
 .shrunk = .base %>% add_extra_columns() %>% rescale_branches() %>% print()
 .shrunk_cols = .shrunk %>% dplyr::select(-parent, -branch.length, -label, -is_tip)
+
+.root = .base %>% dplyr::filter(is.na(.data$branch.length)) %>% dplyr::pull("parent") %>% print()
+.graph = .base[,c(1L, 2L)] %>% as.matrix() %>% igraph::graph_from_edgelist()
+.gdist = igraph::distances(.graph, as.character(.root), mode = "out") %>% as.vector()
+
 .fortified = list(.base, .shrunk) %>%
   as_multiphylo() %>%
   ggtree::fortify() %>%
   dplyr::left_join(.shrunk_cols, by = "node") %>%
+  dplyr::mutate(step = .gdist[.data$node]) %>%
   print()
+
+.p_min = min(.fortified$p_driver, na.rm=TRUE)
+filter_p_driver = function(x) {dplyr::filter(x, p_driver == .p_min)}
 
 .p = ggplot(.fortified, aes(x, y)) +
   geom_tree() +
-  geom_nodepoint(data = function(x) {dplyr::filter(x, p_driver < 0.01)}, colour = "orangered", size = 4) +
+  geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
   geom_tiplab() +
   facet_wrap(~.id, ncol=1) +
   wtl::theme_wtl() +
   wtl::erase(axis.title)
 .p
 ggsave('scaletree.pdf', .p, width=7, height=9.9)
+
+.tree1 = .fortified %>% dplyr::filter(.id == "Tree #1") %>% print()
+.tree2 = .fortified %>% dplyr::filter(.id == "Tree #2") %>% print()
+
+.plot_sequencially = function(df) {
+  .xlim = range(df$x)
+  .ylim = range(df$y)
+  purrr::map(seq_len(max(df$step)), ~{
+    ggplot(df %>% dplyr::filter(step <= .x), aes(x, y)) +
+      geom_tree() +
+      geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
+      geom_tiplab() +
+      coord_cartesian(xlim = .xlim, ylim = .ylim) +
+      theme_void()
+      # wtl::theme_wtl() + wtl::erase(axis.title)
+  })
+}
+.plts = .plot_sequencially(.tree1)
+.tmpl = "~/git/slides-draft/content/tumopp2018ncc/figure-ncc/tree1.%d.png"
+purrr::iwalk(.plts, ~ggsave(sprintf(.tmpl, .y), .x, width=4, height=2))
+
+system("magick -loop 1 -delay 50 tree*.png tree1-raw.gif")
+system("magick -loop 1 tree1-raw.gif -layers Optimize tree1.gif")
+
+.rec_rescale_branches = function(x) {
+  num_edges = nrow(x)
+  l = NULL
+  while (nrow(x) > 1L) {
+    x = nest_tippairs(x)
+    l = c(l, list(x))
+  }
+  purrr::map(l, ~{
+    while (nrow(.x) < num_edges) {
+      .x = unnest_children(.x)
+    }
+    .x %>% dplyr::arrange(node)
+  })
+}
+.shrunkl = .base %>% add_extra_columns() %>% .rec_rescale_branches() %>% print()
+
+.fortified_l = .shrunkl %>% purrr::map(~{
+  .x %>%
+    as_phylo() %>%
+    ggtree::fortify() %>%
+    dplyr::left_join(.shrunk_cols, by = "node")
+})
+
+.plot_scale = function(.x, xlim = .xlim) {
+  ggplot(.x, aes(x, y)) +
+    geom_tree() +
+    geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
+    geom_tiplab() +
+    coord_cartesian(xlim = xlim) +
+    theme_void()
+    # wtl::theme_wtl() + wtl::erase(axis.title)
+}
+
+.xlim = range(.fortified$x) %>% print()
+.plts_scale = .fortified_l %>% purrr::map(.plot_scale)
+.tmpl = "~/git/slides-draft/content/tumopp2018ncc/figure-ncc/scale.%02d.png"
+purrr::iwalk(.plts_scale, ~ggsave(sprintf(.tmpl, .y), .x, width=3, height=1, scale=2.4, dpi=240))
+.final = .fortified_l %>% purrr::pluck(length(.)) %>% .plot_scale(NULL)
+ggsave(sprintf(.tmpl, 99L), .final, width=3, height=1, scale=2.4, dpi=240)
+
+system("magick -loop 1 -delay 50 scale*.png scale-raw.gif")
+system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
