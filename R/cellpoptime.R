83cdf2a80d11ea496e592cfe5d555746635e0a10
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-06-24 21:43:21 +0900

    :wrench: Use "_PACKAGE" instead of docType

--- R/cellpoptime.R
+++ R/cellpoptime.R
@@ -1,6 +1,4 @@
 #' cellpoptime: analyze and visualize phylogenetic tree of cell population
-#' @docType package
-#' @name cellpoptime
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
-NULL
+"_PACKAGE"
