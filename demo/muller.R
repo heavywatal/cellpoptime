2de852f91e4a536f72f865b47c0d93a218cc1ae6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-06-25 18:19:06 +0900

    :construction: Struggle with figures

--- demo/muller.R
+++ demo/muller.R
@@ -1,5 +1,33 @@
 library(tidyverse)
 library(wtl)
+
+make_data = function(tmin, tmax, p=0.4, sd=0.3) {
+    tmean = mean(c(tmax, tmin))
+    tibble(x= seq(tmin, tmax, length.out=20),
+           ymin= p - p * pnorm(x, tmean, sd),
+           ymax= p + (1 - p) * pnorm(x, tmean, sd)) %>%
+    add_row(x=10, ymin=0, ymax=1)
+}
+make_data(2, 4)
+
+.tbl = tibble(tmin=c(0, 3.0), tmax=c(5.5, 5.9), p=c(0.5, 0.8), sd=c(0.55, 0.30)) %>%
+    purrr::pmap_dfr(make_data, .id='id') %>%
+    print()
+
+.p = ggplot(.tbl, aes(x))+
+    geom_ribbon(aes(ymin = ymin, ymax = ymax, fill = id), alpha = 1)+
+    scale_fill_manual(values = c("#f768a1", "#7a0177"), guide = FALSE)+
+    coord_cartesian(xlim = c(0.5, 4.5), ylim = c(0, 1), expand = FALSE)+
+    # labs(x='Time')+
+    wtl::erase(axis.title, axis.text, axis.ticks, panel.grid)
+.p
+
+# .outfile = '~/git/slides-draft/content/tumopp2018ncc/figure-ncc/muller.png'
+.outfile = '~/Dropbox/working/tumopp/cell-diversity-iwate/muller.pdf'
+ggsave(.outfile, .p, width=3, height=1, scale=1.2, dpi=250)
+
+# #######1#########2#########3#########4#########5#########6#########7#########
+
 library(ggmuller)
 
 edges = tibble(Parent = c("A", "A"), Identity = c("B", "C")) %>% print()
--- demo/sandbox.R
+++ demo/sandbox.R
@@ -5,6 +5,9 @@ library(ggtree)
 refresh('rtumopp')
 refresh('cellpoptime')
 
+.wd = "~/Dropbox/working/tumopp/cell-diversity-iwate/"
+setwd(.wd)
+
 .ggtree = function(.tbl) {
   ggtree(.tbl) +
   geom_tree() +
@@ -18,11 +21,22 @@ samples = purrr::rerun(4L, {
   (tumopp::mslike(N, .param) %>% wtl::parse_ms(byrow=TRUE))[[1L]]
 }) %>% print()
 
-N = 2000L
-.param = c('-D2', '-k100', paste0('-N', N), paste0('-U', N / 2L), '--mb=99', '--ms1mut', '-u0')
-samples = purrr::rerun(4L, {
-  (tumopp::mslike(N / 100L, .param) %>% wtl::parse_ms(byrow=TRUE))[[1L]]
-}) %>% print()
+N = 127L
+.param = c('-D2', '-k1000', paste0('-N', N), paste0('-U', N %/% 2L - 1L), '--mb=99', '--ms1mut', '-u0')
+samples_full = purrr::rerun(4L, {
+  (tumopp::mslike(N, .param) %>% wtl::parse_ms(byrow=TRUE))[[1L]]
+})
+wtl::save_as(samples_full)
+# samples_full = readRDS("samples_full.rds")
+
+samples_part = purrr::rerun(4L, {
+  (tumopp::mslike(16L, .param) %>% wtl::parse_ms(byrow=TRUE))[[1L]]
+})
+wtl::save_as(samples_part)
+# samples_part = readRDS("samples_part.rds")
+
+samples = samples_full
+samples = samples_part
 
 # samples = wtl::ms(6L, 4L, theta = 100) %>% wtl::parse_ms() %>% print()
 trees = samples %>% purrr::map(cellpoptime::infer_rooted_tree) %>% print()
@@ -31,123 +45,109 @@ trees %>%
   .ggtree() +
   facet_wrap(~.id)
 
-.tree = trees[[1]]
+.tree = trees[[1L]]
 .base = .tree %>% cellpoptime::remove_outgroup() %>% print()
 
-.base %>% add_extra_columns()
-.base %>% add_extra_columns() %>% filter_scale_tips()
+# refresh('cellpoptime')
+.shrunk = .base %>% add_extra_columns() %>% scale_branches() %>% print()
+.shrunk %>% dplyr::group_by(is_tip) %>% summarise(sum(exp_sibs))
 
-p_binom(c(1, 8))
-detect_driver(c(1, 8))
-
-.base %>% add_extra_columns() %>% nest_tippairs() #%>% nest_tippairs() %>% nest_tippairs()
-
-rescale_branches = function(x) {
-  num_edges = nrow(x)
-  while (nrow(x) > 1L) {
-    x = nest_tippairs(x)
-  }
-  while (nrow(x) < num_edges) {
-    x = unnest_children(x)
-  }
-  x %>% dplyr::arrange(node)
-}
-
-.shrunk = .base %>% add_extra_columns() %>% rescale_branches() %>% print()
-.mutant = .shrunk %>% dplyr::top_n(1L, desc(p_driver)) %>% dplyr::pull(node) %>% print()
+.mutant = .shrunk %>% dplyr::top_n(1L, desc(p_driver)) %>% print()
 
 .shrunk_cols = .shrunk %>%
-  group_clade(.mutant) %>%
+  group_clade(.mutant$node) %>%
   dplyr::select(-parent, -branch.length, -label, -is_tip)
 
 .root = .base %>% dplyr::filter(is.na(.data$branch.length)) %>% dplyr::pull("parent") %>% print()
 .graph = .base[,c(1L, 2L)] %>% igraph::graph_from_data_frame()
-.gdist = igraph::distances(.graph, as.character(.root), mode = "out") %>% as.vector()
+igraph::degree(.graph, mode = "in")
+.gdist = igraph::distances(.graph, as.character(.root), mode = "out") %>%
+  as.vector() %>%
+  setNames(names(igraph::V(.graph))) %>%
+  print()
 
 .fortified = list(.base, .shrunk) %>%
   as_multiphylo() %>%
   ggtree::fortify() %>%
   dplyr::left_join(.shrunk_cols, by = "node") %>%
-  dplyr::mutate(step = .gdist[.data$node]) %>%
+  dplyr::mutate(step = .gdist[as.character(.data$node)]) %>%
   print()
 
-.p_min = min(.fortified$p_driver, na.rm=TRUE)
-filter_p_driver = function(x) {dplyr::filter(x, p_driver == .p_min)}
+.filter_driver_mutation = function(x) {
+  x %>%
+    tidyr::nest(-.id) %>%
+    dplyr::transmute(.data$.id, data = purrr::map(data, ~{
+      row = dplyr::top_n(.x, 1L, dplyr::desc(.data$p_driver))
+      upstream_corner(.x, row$node)
+    })) %>%
+    tidyr::unnest()
+}
+.fortified %>% .filter_driver_mutation()
 
-.p = ggplot(.fortified, aes(x, y)) +
-  geom_tree(aes(colour=group)) +
-  scale_colour_manual(values = c(`0` = '#333333', `1` = 'orangered')) +
-  geom_tiplab() +
-  geom_text2(aes(subset=!isTip, label=node), hjust=-.3) +
-  # geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
-  facet_wrap(~.id, ncol=1) +
-  wtl::theme_wtl() +
-  wtl::erase(axis.title)
-.p
-ggsave('scaletree.pdf', .p, width=7, height=9.9)
+.fortified %>% .plot_snapshot(NULL) + facet_wrap(~.id, ncol = 1L)
+
+.plot_snapshot = function(df, xlim, ylim = NULL, step = 65535) {
+  df %>% dplyr::filter(.data$step <= !!step) %>%
+    dplyr::mutate(isTip = ! node %in% parent) %>%
+    ggplot(aes(x, y)) +
+    geom_tree(aes(colour = group)) +
+    ggtree::geom_tippoint(aes(colour = group), size = 2, alpha = 0.5) +
+    scale_colour_manual(values = c(`0` = "#f768a1", `1` = "#7a0177"), guide = FALSE) +
+    # geom_nodepoint(data = .driver_data, colour = "dodgerblue", size = 4) +
+    # geom_tiplab() +
+    # geom_text2(aes(subset=!isTip, label=node), hjust=-.3) +
+    coord_cartesian(xlim = xlim, ylim = ylim) +
+    theme_void()
+    # wtl::theme_wtl() + wtl::erase(axis.title)
+}
+
+.ggsave = function(filename, plot) {
+  ggsave(filename, plot, width=3, height=1, scale=2.4, dpi=200)
+}
+
+# #######1#########2#########3#########4
 
 .tree1 = .fortified %>% dplyr::filter(.id == "Tree #1") %>% print()
 .tree2 = .fortified %>% dplyr::filter(.id == "Tree #2") %>% print()
 
+.tree1 %>% .plot_snapshot(NULL)
+.tree1 %>% dplyr::filter(step <= 4) %>% .plot_snapshot(NULL)
+
+# in cell-division scale
 .plot_sequencially = function(df) {
   .xlim = range(df$x)
   .ylim = range(df$y)
-  purrr::map(seq_len(max(df$step)), ~{
-    ggplot(df %>% dplyr::filter(step <= .x), aes(x, y)) +
-      geom_tree() +
-      geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
-      geom_tiplab() +
-      coord_cartesian(xlim = .xlim, ylim = .ylim) +
-      theme_void()
-      # wtl::theme_wtl() + wtl::erase(axis.title)
-  })
+  seq_len(max(df$step)) %>%
+    purrr::map(.plot_snapshot, df = df, xlim = .xlim, ylim = .ylim)
 }
+
 .plts = .plot_sequencially(.tree1)
-.tmpl = "~/git/slides-draft/content/tumopp2018ncc/figure-ncc/tree1.%d.png"
-purrr::iwalk(.plts, ~ggsave(sprintf(.tmpl, .y), .x, width=4, height=2))
+.tmpl_g = "scale-genetic-%02d.pdf"
+# .tmpl_g = "part-scale-genetic-%02d.pdf"
+purrr::iwalk(.plts, ~.ggsave(sprintf(.tmpl_g, .y), .x))
 
 system("magick -loop 1 -delay 50 tree*.png tree1-raw.gif")
 system("magick -loop 1 tree1-raw.gif -layers Optimize tree1.gif")
 
-.rec_rescale_branches = function(x) {
-  num_edges = nrow(x)
-  l = NULL
-  while (nrow(x) > 1L) {
-    x = nest_tippairs(x)
-    l = c(l, list(x))
-  }
-  purrr::map(l, ~{
-    while (nrow(.x) < num_edges) {
-      .x = unnest_children(.x)
-    }
-    .x %>% dplyr::arrange(node)
-  })
-}
-.shrunkl = .base %>% add_extra_columns() %>% .rec_rescale_branches() %>% print()
+# #######1#########2#########3#########4
+
+.shrunkl = .base %>% add_extra_columns() %>% scale_branches_record() %>% print()
 
 .fortified_l = .shrunkl %>% purrr::map(~{
   .x %>%
     as_phylo() %>%
     ggtree::fortify() %>%
-    dplyr::left_join(.shrunk_cols, by = "node")
+    dplyr::left_join(.shrunk_cols, by = "node") %>%
+    dplyr::mutate(step = .gdist[as.character(.data$node)])
 })
 
-.plot_scale = function(.x, xlim) {
-  ggplot(.x, aes(x, y)) +
-    geom_tree() +
-    geom_nodepoint(data = filter_p_driver, colour = "orangered", size = 4) +
-    geom_tiplab() +
-    coord_cartesian(xlim = xlim) +
-    theme_void()
-    # wtl::theme_wtl() + wtl::erase(axis.title)
-}
-
 .xlim = range(.fortified$x) %>% print()
-.plts_scale = .fortified_l %>% purrr::map(.plot_scale, xlim = .xlim)
-.tmpl = "~/git/slides-draft/content/tumopp2018ncc/figure-ncc/scale.%02d.png"
-purrr::iwalk(.plts_scale, ~ggsave(sprintf(.tmpl, .y), .x, width=3, height=1, scale=2.4, dpi=240))
-.final = .fortified_l %>% purrr::pluck(length(.)) %>% .plot_scale(xlim = NULL)
-ggsave(sprintf(.tmpl, 99L), .final, width=3, height=1, scale=2.4, dpi=240)
+.plts_scale = .fortified_l %>% purrr::map(.plot_snapshot, xlim = .xlim)
+.tmpl_t = "scale-time-%02d.pdf"
+# .tmpl_t = "part-scale-time-%02d.pdf"
+purrr::iwalk(.plts_scale, ~.ggsave(sprintf(.tmpl_t, .y), .x))
+.p_final = .fortified_l %>% purrr::pluck(length(.)) %>% .plot_snapshot(xlim = NULL)
+.ggsave(sprintf(.tmpl_t, 90L), .p_final)
 
 system("magick -loop 1 -delay 50 scale*.png scale-raw.gif")
 system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
@@ -155,21 +155,23 @@ system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
 # #######1#########2#########3#########4#########5#########6#########7#########
 # Triangle
 
+refresh("cellpoptime")
+
 .final_tbl = .fortified_l %>% purrr::pluck(length(.)) %>% print()
 
-.final_tbl %>% .plot_scale(xlim = NULL)
-
-.source = .final_tbl %>% dplyr::filter(parent == node) %>% dplyr::pull(node) %>% print()
-.mutantp = .final_tbl %>% dplyr::filter(node == .mutant) %>% dplyr::pull(parent) %>% print()
-.fastnodes = tumopp:::paths_to_sink(.graph, as.character(.mutant))[[1L]] %>% as.integer() %>% print()
+.fastnodes = tumopp:::paths_to_sink(.graph, as.character(.mutant$node))[[1L]] %>% as.integer() %>% print()
 .fastvertices = .final_tbl %>%
   dplyr::filter(isTip, node %in% .fastnodes) %>%
   dplyr::filter(y %in% range(y)) %>%
   dplyr::pull(node) %>%
   print()
 
+.joint = .final_tbl %>% upstream_corner(.mutant$node) %>% print()
+
 .fast_tbl = .final_tbl %>%
-  dplyr::filter(x, node %in% c(.fastvertices, .mutant)) %>%
+  dplyr::filter(node %in% .fastvertices) %>%
+  dplyr::mutate(y = y + ifelse(y < mean(y), -0.5, 0.5)) %>%
+  dplyr::bind_rows(.joint) %>%
   print()
 
 .slowvertices = .final_tbl %>%
@@ -178,10 +180,22 @@ system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
   dplyr::pull(node) %>%
   print()
 
-.slow_tbl = .final_tbl %>%
-  dplyr::filter(.data$node %in% c(.slowvertices, .mutantp, .source)) %>%
+.mrca = .final_tbl %>%
+  dplyr::filter(parent == node) %>%
+  # dplyr::mutate(x = -0.5 * max(.final_tbl$x)) %>%
   print()
 
-.final_tbl %>% .plot_scale(xlim = NULL) +
-  geom_polygon(data = .fast_tbl, alpha = 0.5, fill = "#7a0177")+
-  geom_polygon(data = .slow_tbl, alpha = 0.5, fill = "#f768a1")
+.slow_tbl = .final_tbl %>%
+  dplyr::filter(.data$node %in% .slowvertices) %>%
+  dplyr::mutate(y = y + ifelse(y < mean(y), -0.5, 0.5)) %>%
+  dplyr::arrange(desc(y)) %>%
+  dplyr::bind_rows(.mrca, .joint) %>%
+  print()
+
+.p_triangle = .final_tbl %>%
+  .plot_snapshot(xlim = NULL) +
+  # geom_point(aes(size = exp_sibs), alpha = 0.4) +
+  geom_polygon(data = .fast_tbl, alpha = 0.6, fill = "#7a0177")+
+  geom_polygon(data = .slow_tbl, alpha = 0.6, fill = "#f768a1")
+.p_triangle
+.ggsave(sprintf(.tmpl_t, 99L), .p_triangle)
