658a655f077fcacadf2eaea1ab3d8c66954a0538
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-06-26 15:50:21 +0900

    :construction: Use x to draw partial trees

--- demo/sandbox.R
+++ demo/sandbox.R
@@ -1,19 +1,11 @@
 library(tidyverse)
 library(wtl)
-library(tumopp)
 library(ggtree)
-refresh('rtumopp')
-refresh('cellpoptime')
+wtl::refresh('rtumopp')
+wtl::refresh('cellpoptime')
 
 .wd = "~/Dropbox/working/tumopp/cell-diversity-iwate/"
 setwd(.wd)
-
-.ggtree = function(.tbl) {
-  ggtree(.tbl) +
-  geom_tree() +
-  geom_tiplab() +
-  wtl::theme_wtl()
-}
 
 N = 20L
 .param = c('-D2', '-k100', paste0('-N', N), paste0('-U', N / 2L), '--mb=99', '--ms1mut', '-u0')
@@ -42,52 +34,36 @@ samples = samples_part
 trees = samples %>% purrr::map(cellpoptime::infer_rooted_tree) %>% print()
 trees %>%
   cellpoptime::as_multiphylo() %>%
-  .ggtree() +
+  ggtree() +
+  geom_tree() +
+  geom_tiplab() +
+  wtl::theme_wtl() +
   facet_wrap(~.id)
 
 .tree = trees[[1L]]
 .base = .tree %>% cellpoptime::remove_outgroup() %>% print()
 
-# refresh('cellpoptime')
-.shrunk = .base %>% add_extra_columns() %>% scale_branches() %>% print()
+.scaled_rec = .base %>% scale_branches_record()
+.shrunk = .scaled_rec %>% dplyr::last() %>% print()
+# .shrunk = .base %>% scale_branches() %>% print()
 .shrunk %>% dplyr::group_by(is_tip) %>% summarise(sum(exp_sibs))
 
-.mutant = .shrunk %>% dplyr::top_n(1L, desc(p_driver)) %>% print()
-
-.shrunk_cols = .shrunk %>%
+.mutant = .shrunk %>% dplyr::top_n(1L, dplyr::desc(p_driver)) %>% print()
+.meta_info = .shrunk %>%
   group_clade(.mutant$node) %>%
-  dplyr::select(-parent, -branch.length, -label, -is_tip)
-
-.root = .base %>% dplyr::filter(is.na(.data$branch.length)) %>% dplyr::pull("parent") %>% print()
-.graph = .base[,c(1L, 2L)] %>% igraph::graph_from_data_frame()
-igraph::degree(.graph, mode = "in")
-.gdist = igraph::distances(.graph, as.character(.root), mode = "out") %>%
-  as.vector() %>%
-  setNames(names(igraph::V(.graph))) %>%
+  dplyr::select(-parent, -branch.length, -label, -is_tip) %>%
   print()
 
-.fortified = list(.base, .shrunk) %>%
+.fortified = .shrunkl %>%
   as_multiphylo() %>%
   ggtree::fortify() %>%
-  dplyr::left_join(.shrunk_cols, by = "node") %>%
-  dplyr::mutate(step = .gdist[as.character(.data$node)]) %>%
+  dplyr::left_join(.meta_info, by = "node") %>%
   print()
 
-.filter_driver_mutation = function(x) {
-  x %>%
-    tidyr::nest(-.id) %>%
-    dplyr::transmute(.data$.id, data = purrr::map(data, ~{
-      row = dplyr::top_n(.x, 1L, dplyr::desc(.data$p_driver))
-      upstream_corner(.x, row$node)
-    })) %>%
-    tidyr::unnest()
-}
-.fortified %>% .filter_driver_mutation()
+.fortified %>% .plot_snapshot() + facet_wrap(~.id, ncol = 1L)
 
-.fortified %>% .plot_snapshot(NULL) + facet_wrap(~.id, ncol = 1L)
-
-.plot_snapshot = function(df, xlim, ylim = NULL, step = 65535) {
-  df %>% dplyr::filter(.data$step <= !!step) %>%
+.plot_snapshot = function(df, xlim = NULL, ylim = NULL, xmax = 65535) {
+  df %>% dplyr::filter(.data$x <= xmax) %>%
     dplyr::mutate(isTip = ! node %in% parent) %>%
     ggplot(aes(x, y)) +
     geom_tree(aes(colour = group)) +
@@ -106,22 +82,35 @@ igraph::degree(.graph, mode = "in")
 }
 
 # #######1#########2#########3#########4
+# Draw scaling steps
 
-.tree1 = .fortified %>% dplyr::filter(.id == "Tree #1") %>% print()
-.tree2 = .fortified %>% dplyr::filter(.id == "Tree #2") %>% print()
+.xlim = range(.fortified$x) %>% print()
+.nested = .fortified %>% tidyr::nest(-.id) %>% print()
+.plts_scale = .nested$data %>% purrr::map(.plot_snapshot, xlim = .xlim)
+.tmpl_t = "scale-time-%02d.pdf"
+# .tmpl_t = "part-scale-time-%02d.pdf"
+purrr::iwalk(.plts_scale, ~.ggsave(sprintf(.tmpl_t, .y), .x))
+.p_final = .nested$data %>% purrr::pluck(length(.)) %>% .plot_snapshot()
+.ggsave(sprintf(.tmpl_t, 90L), .p_final)
 
-.tree1 %>% .plot_snapshot(NULL)
-.tree1 %>% dplyr::filter(step <= 4) %>% .plot_snapshot(NULL)
+system("magick -loop 1 -delay 50 scale*.png scale-raw.gif")
+system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
 
-# in cell-division scale
+# #######1#########2#########3#########4
+# Draw partial tree from small x
+
+.fortified1 = .nested$data[[1L]] %>% print()
+.fortified1 %>% .plot_snapshot(NULL)
+.fortified1 %>% dplyr::filter(x <= 3) %>% .plot_snapshot()
+
 .plot_sequencially = function(df) {
   .xlim = range(df$x)
   .ylim = range(df$y)
-  seq_len(max(df$step)) %>%
+  unique(df$x) %>% sort() %>% tail(-1L) %>%
     purrr::map(.plot_snapshot, df = df, xlim = .xlim, ylim = .ylim)
 }
 
-.plts = .plot_sequencially(.tree1)
+.plts = .plot_sequencially(.fortified1)
 .tmpl_g = "scale-genetic-%02d.pdf"
 # .tmpl_g = "part-scale-genetic-%02d.pdf"
 purrr::iwalk(.plts, ~.ggsave(sprintf(.tmpl_g, .y), .x))
@@ -129,70 +118,46 @@ purrr::iwalk(.plts, ~.ggsave(sprintf(.tmpl_g, .y), .x))
 system("magick -loop 1 -delay 50 tree*.png tree1-raw.gif")
 system("magick -loop 1 tree1-raw.gif -layers Optimize tree1.gif")
 
-# #######1#########2#########3#########4
-
-.shrunkl = .base %>% add_extra_columns() %>% scale_branches_record() %>% print()
-
-.fortified_l = .shrunkl %>% purrr::map(~{
-  .x %>%
-    as_phylo() %>%
-    ggtree::fortify() %>%
-    dplyr::left_join(.shrunk_cols, by = "node") %>%
-    dplyr::mutate(step = .gdist[as.character(.data$node)])
-})
-
-.xlim = range(.fortified$x) %>% print()
-.plts_scale = .fortified_l %>% purrr::map(.plot_snapshot, xlim = .xlim)
-.tmpl_t = "scale-time-%02d.pdf"
-# .tmpl_t = "part-scale-time-%02d.pdf"
-purrr::iwalk(.plts_scale, ~.ggsave(sprintf(.tmpl_t, .y), .x))
-.p_final = .fortified_l %>% purrr::pluck(length(.)) %>% .plot_snapshot(xlim = NULL)
-.ggsave(sprintf(.tmpl_t, 90L), .p_final)
-
-system("magick -loop 1 -delay 50 scale*.png scale-raw.gif")
-system("magick -loop 1 scale-raw.gif -layers Optimize scale.gif")
-
 # #######1#########2#########3#########4#########5#########6#########7#########
 # Triangle
 
 refresh("cellpoptime")
 
-.final_tbl = .fortified_l %>% purrr::pluck(length(.)) %>% print()
+.fortified_last = .nested$data %>% purrr::pluck(length(.)) %>% print()
 
-.fastnodes = tumopp:::paths_to_sink(.graph, as.character(.mutant$node))[[1L]] %>% as.integer() %>% print()
-.fastvertices = .final_tbl %>%
-  dplyr::filter(isTip, node %in% .fastnodes) %>%
+.fastvertices = .fortified_last %>%
+  dplyr::filter(isTip, group == 1) %>%
   dplyr::filter(y %in% range(y)) %>%
   dplyr::pull(node) %>%
   print()
 
-.joint = .final_tbl %>% upstream_corner(.mutant$node) %>% print()
+.joint = .fortified_last %>% upstream_corner(.mutant$node) %>% print()
 
-.fast_tbl = .final_tbl %>%
+.fast_tbl = .fortified_last %>%
   dplyr::filter(node %in% .fastvertices) %>%
   dplyr::mutate(y = y + ifelse(y < mean(y), -0.5, 0.5)) %>%
   dplyr::bind_rows(.joint) %>%
   print()
 
-.slowvertices = .final_tbl %>%
-  dplyr::filter(isTip, !node %in% .fastnodes) %>%
+.slowvertices = .fortified_last %>%
+  dplyr::filter(isTip, group == 0) %>%
   dplyr::filter(y %in% range(y)) %>%
   dplyr::pull(node) %>%
   print()
 
-.mrca = .final_tbl %>%
+.mrca = .fortified_last %>%
   dplyr::filter(parent == node) %>%
-  # dplyr::mutate(x = -0.5 * max(.final_tbl$x)) %>%
+  # dplyr::mutate(x = -0.5 * max(.fortified_last$x)) %>%
   print()
 
-.slow_tbl = .final_tbl %>%
+.slow_tbl = .fortified_last %>%
   dplyr::filter(.data$node %in% .slowvertices) %>%
   dplyr::mutate(y = y + ifelse(y < mean(y), -0.5, 0.5)) %>%
   dplyr::arrange(desc(y)) %>%
   dplyr::bind_rows(.mrca, .joint) %>%
   print()
 
-.p_triangle = .final_tbl %>%
+.p_triangle = .fortified_last %>%
   .plot_snapshot(xlim = NULL) +
   # geom_point(aes(size = exp_sibs), alpha = 0.4) +
   geom_polygon(data = .fast_tbl, alpha = 0.6, fill = "#7a0177")+
