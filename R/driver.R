bf26c2105a42285a04be58b4cd78a049fbd76002
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-11-13 17:29:26 +0900

    :construction: Add mutant_labels()

--- R/driver.R
+++ R/driver.R
@@ -17,3 +17,15 @@ detect_driver = function(v) {
 filter_origins = function(x, method = "fdr", q = 0.05) {
   dplyr::filter(x, stats::p.adjust(.data$p_driver, method = method) < q)
 }
+
+#' @description
+#' `mutant_labels` extracts mutated nodes in tbl_tree
+#' @rdname driver
+#' @export
+mutant_labels = function(x) {
+  origins = filter_origins(x)$node
+  group_clade(x, origins) %>%
+    dplyr::filter(.data$is_tip, .data$group != 0L) %>%
+    dplyr::pull("label") %>%
+    as.integer()
+}
