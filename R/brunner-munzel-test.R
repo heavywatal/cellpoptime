9e54cd83753137145d60b540e87210ad931acf3a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-01-23 20:30:03 +0900

    :art: Clean brunner.munzel.test()

--- R/brunner-munzel-test.R
+++ R/brunner-munzel-test.R
@@ -1,52 +1,38 @@
 # Copied from lawstat package on 2019-01-23
 # https://github.com/vlyubchich/lawstat/blob/master/lawstat/R/brunner.munzel.test.R
-brunner.munzel.test<-function (x, y, alternative = c("two.sided", "greater", "less"),
-    alpha = 0.05)
-{
-    alternative <- match.arg(alternative)
-    DNAME = paste(deparse(substitute(x)), "and", deparse(substitute(y)))
-    x <- na.omit(x)
-    y <- na.omit(y)
-    n1 = length(x)
-    n2 = length(y)
-    r1 = rank(x)
-    r2 = rank(y)
-    r = rank(c(x, y))
-    m1 = mean(r[1:n1])
-    m2 = mean(r[n1 + 1:n2])
-    pst = (m2 - (n2 + 1)/2)/n1
-    v1 = sum((r[1:n1] - r1 - m1 + (n1 + 1)/2)^2)/(n1 - 1)
-    v2 = sum((r[n1 + 1:n2] - r2 - m2 + (n2 + 1)/2)^2)/(n2 - 1)
-    statistic = n1 * n2 * (m2 - m1)/(n1 + n2)/sqrt(n1 * v1 +
-        n2 * v2)
-    dfbm = ((n1 * v1 + n2 * v2)^2)/(((n1 * v1)^2)/(n1 - 1) +
-        ((n2 * v2)^2)/(n2 - 1))
-    if ((alternative == "greater") | (alternative == "g")) {
-        p.value = pt(statistic, dfbm)
-    }
-    else if ((alternative == "less") | (alternative == "l")) {
-        p.value = 1-pt(statistic, dfbm)
-    }
-    else {
-        alternative = "two.sided"
-        p.value = 2 * min(pt(abs(statistic), dfbm), (1 - pt(abs(statistic),
-            dfbm)))
-    }
-    conf.int = c(pst - qt(1 - alpha/2, dfbm) * sqrt(v1/(n1 *
-        n2^2) + v2/(n2 * n1^2)), pst + qt(1 - alpha/2, dfbm) *
-        sqrt(v1/(n1 * n2^2) + v2/(n2 * n1^2)))
-    estimate = pst
-    ESTIMATE = pst
-    names(ESTIMATE) = "P(X<Y)+.5*P(X=Y)"
-    STATISTIC = statistic
-    names(STATISTIC) = "Brunner-Munzel Test Statistic"
-    PARAMETER = dfbm
-    names(PARAMETER) = "df"
-    CONF.INT = conf.int
-    names(CONF.INT) = c("lower", "upper")
-    attr(CONF.INT, "conf.level") = (1 - alpha)
-    METHOD = "Brunner-Munzel Test"
-    structure(list(estimate = ESTIMATE, conf.int = CONF.INT,
-        statistic = STATISTIC, parameter = PARAMETER, p.value = p.value,
-        method = METHOD, data.name = DNAME), class = "htest")
+# Cleaned by @heavywatal
+brunner.munzel.test = function(x, y, alternative = c("two.sided", "greater", "less"), alpha = 0.05) {
+  alternative = match.arg(alternative)
+  data.name = paste(deparse(substitute(x)), "and", deparse(substitute(y)))
+  x = stats::na.omit(x)
+  y = stats::na.omit(y)
+  n1 = length(x)
+  n2 = length(y)
+  r1 = rank(x)
+  r2 = rank(y)
+  r = rank(c(x, y))
+  m1 = mean(r[seq_len(n1)])
+  m2 = mean(r[-seq_len(n1)])
+  v1 = sum((r[seq_len(n1)] - r1 - m1 + (n1 + 1) / 2)^2) / (n1 - 1)
+  v2 = sum((r[-seq_len(n1)] - r2 - m2 + (n2 + 1) / 2)^2) / (n2 - 1)
+  statistic = n1 * n2 * (m2 - m1) / (n1 + n2) / sqrt(n1 * v1 + n2 * v2)
+  df = ((n1 * v1 + n2 * v2)^2) / (((n1 * v1)^2) / (n1 - 1) + ((n2 * v2)^2) / (n2 - 1))
+  p.value = switch(alternative,
+    two.sided = 2 * stats::pt(abs(statistic), df, lower.tail = FALSE),
+    greater = stats::pt(statistic, df, lower.tail = TRUE),
+    less = stats::pt(statistic, df, lower.tail = FALSE)
+  )
+  estimate = (m2 - (n2 + 1) / 2) / n1
+  conf_half = stats::qt(1 - alpha / 2, df) * sqrt(v1 / (n1 * n2^2) + v2 / (n2 * n1^2))
+  conf.int = estimate + c(-conf_half, conf_half)
+  attr(conf.int, "conf.level") = (1 - alpha)
+  structure(list(
+    statistic = stats::setNames(statistic, "Brunner-Munzel Test Statistic"),
+    parameter = stats::setNames(df, "df"),
+    p.value = p.value,
+    conf.int = stats::setNames(conf.int, c("lower", "upper")),
+    estimate = stats::setNames(estimate, "P(X<Y)+.5*P(X=Y)"),
+    method = "Brunner-Munzel Test",
+    data.name = data.name
+  ), class = "htest")
 }
